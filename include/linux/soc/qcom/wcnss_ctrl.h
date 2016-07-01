ade050de7168acd80437d5d59072d6a0106853f8
Bjorn Andersson <bjorn.andersson@linaro.org>
2016-06-30 22:47:11 -0700

    misc: Transition drivers from smd to rpmsg
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- include/linux/soc/qcom/wcnss_ctrl.h
+++ include/linux/soc/qcom/wcnss_ctrl.h
@@ -1,8 +1,8 @@
 #ifndef __WCNSS_CTRL_H__
 #define __WCNSS_CTRL_H__
 
-#include <linux/soc/qcom/smd.h>
+#include <linux/rpmsg.h>
 
-struct qcom_smd_channel *qcom_wcnss_open_channel(void *wcnss, const char *name, qcom_smd_cb_t cb);
+struct rpmsg_endpoint *qcom_wcnss_open_channel(void *wcnss, const char *name, rpmsg_rx_cb_t cb, void *priv);
 
 #endif
