5b3fe11d6c52474f96cdbf5ebd9011854febd106
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2016-11-04 12:36:08 +0000

    PCI: qcom: add runtime pm support to pcie_port
    This patch is required when the pcie controller sits on a bus with
    its own power domain and clocks which are controlled via a bus driver
    like simple pm bus. As these bus driver have runtime pm enabled, it makes
    sense to update the usage counter so that the runtime pm does not suspend
    the clks or power domain associated with the bus driver.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/pci/dwc/pcie-qcom.c
+++ drivers/pci/dwc/pcie-qcom.c
@@ -897,6 +897,8 @@ static void qcom_pcie_host_init(struct pcie_port *pp)
 	struct qcom_pcie *pcie = to_qcom_pcie(pci);
 	int ret;
 
+	pm_runtime_get_sync(pci->dev);
+
 	qcom_ep_reset_assert(pcie);
 
 	ret = pcie->ops->init(pcie);
@@ -927,6 +929,7 @@ err:
 	phy_power_off(pcie->phy);
 err_deinit:
 	pcie->ops->deinit(pcie);
+	pm_runtime_put_sync(pci->dev);
 }
 
 static int qcom_pcie_rd_own_conf(struct pcie_port *pp, int where, int size,
@@ -992,6 +995,7 @@ static int qcom_pcie_probe(struct platform_device *pdev)
 	struct qcom_pcie *pcie;
 	int ret;
 
+
 	pcie = devm_kzalloc(dev, sizeof(*pcie), GFP_KERNEL);
 	if (!pcie)
 		return -ENOMEM;
@@ -999,6 +1003,8 @@ static int qcom_pcie_probe(struct platform_device *pdev)
 	pci = devm_kzalloc(dev, sizeof(*pci), GFP_KERNEL);
 	if (!pci)
 		return -ENOMEM;
+
+	pm_runtime_enable(dev);
 
 	pci->dev = dev;
 	pci->ops = &dw_pcie_ops;
