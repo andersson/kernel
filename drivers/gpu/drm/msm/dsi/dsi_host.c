freedreno-zz/kernel-msm

drm: msm: dsi: Enable s0_axi & mmss_noc clocks
The s0_axi & mmss_noc clocks are downstream handled by msm_bus, as we
don't have that we have to explicitly enable them from the dsi driver
for now.

Signed-off-by: Bjorn Andersson <bjorn.andersson@sonymobile.com>

drivers/gpu/drm/msm/dsi/dsi_host.c
@@ -372,6 +372,21 @@ static int dsi_clk_init(struct msm_dsi_host *msm_host)
 		goto exit;
 	}
 
+	struct clk *s0_clk;
+	struct clk *mmssnoc_clk;
+
+#if 1
+	mmssnoc_clk = devm_clk_get(dev, "mmssnoc_clk");
+	clk_set_rate(mmssnoc_clk, 100000000);
+	clk_prepare_enable(mmssnoc_clk);
+#endif
+
+#if 1
+	s0_clk = devm_clk_get(dev, "s0_axi_clk");
+	clk_set_rate(s0_clk, 100000000);
+	clk_prepare_enable(s0_clk);
+#endif
+
 exit:
 	return ret;
 }

andersson/kernel

drivers/gpu/drm/msm/dsi/dsi_host.c
@@ -337,6 +337,8 @@ static int dsi_clk_init(struct msm_dsi_host *msm_host)
 	struct platform_device *pdev = msm_host->pdev;
 	const struct msm_dsi_cfg_handler *cfg_hnd = msm_host->cfg_hnd;
 	const struct msm_dsi_config *cfg = cfg_hnd->cfg;
+	struct clk *s0_clk;
+	struct clk *mmssnoc_clk;
 	int i, ret = 0;
 
 	/* get bus clocks */
@@ -418,6 +420,19 @@ static int dsi_clk_init(struct msm_dsi_host *msm_host)
 				__func__, ret);
 		}
 	}
+
+#if 1
+	mmssnoc_clk = devm_clk_get(dev, "mmssnoc");
+	clk_set_rate(mmssnoc_clk, 100000000);
+	clk_prepare_enable(mmssnoc_clk);
+#endif
+
+#if 1
+	s0_clk = devm_clk_get(dev, "s0_axi");
+	clk_set_rate(s0_clk, 100000000);
+	clk_prepare_enable(s0_clk);
+#endif
+
 exit:
 	return ret;
 }