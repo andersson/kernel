b920e72f78923ef585c181bce11d979e9f3642b8
Bjorn Andersson <bjorn.andersson@linaro.org>
2018-01-15 20:53:02 -0800

    Revert "brcmfmac: Make sure CLM downloading is optional"
    This reverts commit 618a6a7d9567ab846db13a4a4492f35a67c9d88c.

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.c
@@ -278,8 +278,12 @@ int brcmf_c_preinit_dcmds(struct brcmf_if *ifp)
 	}
 	ri->result = err;
 
-	/* Do any optional CLM downloading */
-	brcmf_c_process_clm_blob(ifp);
+	/* Do any CLM downloading */
+	err = brcmf_c_process_clm_blob(ifp);
+	if (err < 0) {
+		brcmf_err("download CLM blob file failed, %d\n", err);
+		goto done;
+	}
 
 	/* query for 'ver' to get version info from firmware */
 	memset(buf, 0, sizeof(buf));
