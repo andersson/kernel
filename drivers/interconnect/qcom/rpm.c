d064d10e2b54cd22d5ab7a067eadf60a5a6946f5
Georgi Djakov <georgi.djakov@linaro.org>
2018-02-05 19:58:52 +0200

    interconnect: qcom: Add RPM communication
    Add a driver for communicating with the remote processor, which controls
    some of the Network-On-Chip resources.
    
    Signed-off-by: Georgi Djakov <georgi.djakov@linaro.org>
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

---
+++ drivers/interconnect/qcom/rpm.c
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * RPM communication wrapper for interconects
+ *
+ * Copyright (C) 2017 Linaro Ltd
+ * Author: Georgi Djakov <georgi.djakov@linaro.org>
+ */
+
+#include <linux/interconnect-provider.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/of_platform.h>
+#include <linux/platform_device.h>
+#include <linux/soc/qcom/smd-rpm.h>
+#include "rpm.h"
+
+#define	RPM_KEY_BW	0x00007762
+
+struct qcom_interconnect_rpm interconnect_rpm;
+
+struct interconnect_rpm_req {
+	__le32 key;
+	__le32 nbytes;
+	__le32 value;
+};
+
+int qcom_interconnect_rpm_send(int ctx, int rsc_type, int id, u32 val)
+{
+	struct interconnect_rpm_req req = {
+		.key = cpu_to_le32(RPM_KEY_BW),
+		.nbytes = cpu_to_le32(sizeof(u32)),
+		.value = cpu_to_le32(val),
+	};
+
+	if (!interconnect_rpm.rpm) {
+		pr_err("%s: RPM not ready yet\n", __func__);
+		return -EBUSY;
+	}
+
+	return qcom_rpm_smd_write(interconnect_rpm.rpm, ctx, rsc_type, id, &req, sizeof(req));
+}
+EXPORT_SYMBOL(qcom_interconnect_rpm_send);
+
+static int qcom_interconnect_rpm_probe(struct platform_device *pdev)
+{
+	interconnect_rpm.rpm = dev_get_drvdata(pdev->dev.parent);
+	if (!interconnect_rpm.rpm) {
+		dev_err(&pdev->dev, "unable to retrieve handle to rpm\n");
+		return -ENODEV;
+	}
+
+	pr_info("interconnect: initialized RPM communication channel\n");
+
+	return 0;
+}
+
+static const struct of_device_id qcom_interconnect_rpm_dt_match[] = {
+	{ .compatible = "qcom,interconnect-rpm", },
+	{ },
+};
+
+MODULE_DEVICE_TABLE(of, qcom_interconnect_rpm_dt_match);
+
+static struct platform_driver qcom_interconnect_rpm_driver = {
+	.driver = {
+		.name		= "qcom-interconnect-rpm",
+		.of_match_table	= qcom_interconnect_rpm_dt_match,
+	},
+	.probe = qcom_interconnect_rpm_probe,
+};
+
+module_platform_driver(qcom_interconnect_rpm_driver);
---
+++ drivers/interconnect/qcom/rpm.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (c) 2018, Linaro Ltd.
+ * Author: Georgi Djakov <georgi.djakov@linaro.org>
+ */
+
+#ifndef __LINUX_INTERCONNECT_QCOM_RPM_H
+#define __LINUX_INTERCONNECT_QCOM_RPM_H
+
+#include <linux/soc/qcom/smd-rpm.h>
+
+struct qcom_interconnect_rpm {
+	struct qcom_smd_rpm *rpm;
+};
+
+extern struct qcom_interconnect_rpm interconnect_rpm;
+
+int qcom_interconnect_rpm_send(int ctx, int rsc_type, int id, u32 val);
+
+#endif
