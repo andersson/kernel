0b43b3d4f849d9a4287e56d754afb80be3965344
Bjorn Andersson <bjorn.andersson@linaro.org>
2019-02-13 20:12:47 -0800

    arm64: dts: qcom: Fully describe the CDSP
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- init/initramfs.c
+++ init/initramfs.c
@@ -610,14 +610,14 @@ static void __init clean_rootfs(void)
 
 static int __initdata do_skip_initramfs;
 
-static int __init skip_initramfs_param(char *str)
-{
-	if (*str)
-		return 0;
-	do_skip_initramfs = 1;
-	return 1;
-}
-__setup("skip_initramfs", skip_initramfs_param);
+//static int __init skip_initramfs_param(char *str)
+//{
+//	if (*str)
+//		return 0;
+//	do_skip_initramfs = 1;
+//	return 1;
+//}
+//__setup("skip_initramfs", skip_initramfs_param);
 
 static int __init populate_rootfs(void)
 {
