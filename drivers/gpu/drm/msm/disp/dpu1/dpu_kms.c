98071f0f281f7d53d20d34e126616454adb63980
Bjorn Andersson <bjorn.andersson@linaro.org>
2021-03-16 08:19:52 -0700

    drm: msm; bits and pieces for sc8180x
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_kms.c
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_kms.c
@@ -1219,6 +1219,7 @@ static const struct dev_pm_ops dpu_pm_ops = {
 static const struct of_device_id dpu_dt_match[] = {
 	{ .compatible = "qcom,sdm845-dpu", },
 	{ .compatible = "qcom,sc7180-dpu", },
+	{ .compatible = "qcom,sc8180x-dpu", },
 	{}
 };
 MODULE_DEVICE_TABLE(of, dpu_dt_match);
