cb1b2b76728f773f5d3ba15b05d5dede1e5c0f76
Bhupesh Sharma <bhupesh.sharma@linaro.org>
2021-11-29 13:04:41 +0530

    net: ethernet: stmmac: Disable pm_runtime for stmmmac driver for now
    Signed-off-by: Bhupesh Sharma <bhupesh.sharma@linaro.org>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -7160,6 +7160,11 @@ int stmmac_dvr_probe(struct device *device,
 	pm_runtime_get_noresume(device);
 	pm_runtime_set_active(device);
 	pm_runtime_enable(device);
+	/*
+	 * Prevent runtime pm from being ON by default. Users can enable
+	 * it using power/control in sysfs.
+	 */
+	pm_runtime_forbid(device);
 
 	if (priv->hw->pcs != STMMAC_PCS_TBI &&
 	    priv->hw->pcs != STMMAC_PCS_RTBI) {
